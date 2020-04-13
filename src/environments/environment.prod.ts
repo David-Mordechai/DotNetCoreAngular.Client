a493898a556bebac84dc31d7f747a0110c92a3cf
David Mordechai <david.mordechai79@gmail.com>
2020-04-14 00:17:26 +0300

    fixing prod apiurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'https://localhost/DotNetCoreAngular.WebApi/api/'
+  baseApiUrl: 'https://davidmordechai.com/DotNetCoreAngular.WebApi/api/'
 };
